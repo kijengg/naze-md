8a8115597b7fc1a2b0929da3d16392e0998e6b1a
kijengg <115552596+kijengg@users.noreply.github.com>
2022-10-16 13:14:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
+global.owner = ['KyzheBOT']
+global.ownernomer = "6288991344802"
 global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
+global.packname = 'Sticker  Kyzhe'
 global.author = 'Naze'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
